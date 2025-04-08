83709a62d00523f4e58bca393ec6098900a436d6
ravi kumar <ravi.kumar07@telusinternational.com>
2025-04-08 18:30:48 +0530

    added basic projects lisr

---
+++ Training/fullstack-projects.js
@@ -0,0 +1,14 @@
+/*
+    Basic Projects:
+    1) To-Do List App:
+    2) Calculator 
+    3) Image Gallery with Modal view
+
+    Intermediate Projects:
+    1) Quiz app
+    2) Weather App
+
+    Advance Projects: 
+    1) Blogging Website:
+    2) Movie App (using TMDB or OMDB API)
+*/
